92db419d55c396ca1806dc05ef18ae2752788f71
Jaehyun Chi <jaehchi@gmail.com>
2018-10-18 15:07:09 -0700

    [ fix ] - fixed user query

--- gql-server/src/config/graphQL/resolvers/queries/user.js
+++ gql-server/src/config/graphQL/resolvers/queries/user.js
@@ -1,13 +1,12 @@
 import { getUserID } from '../../../utils/jwt';
-import { user } from '../authPayload';
 
 export default async (parent, { id }, ctx, info) => {
   let user; 
   
   if (id) { // if args.id is defined, then we're looking for a specific user
-    user = await getUserID(ctx.request);
-  } else { // else we're looking for the current user
     user = id;
+  } else { // else we're looking for the current user
+    user = await getUserID(ctx.request);
   }
 
   return await ctx.db.query.users({ 
