aa20df5e86d649e09c3a31b110e9bec9b109bddf
Jaehyun Chi <jaehchi@gmail.com>
2018-10-24 18:55:13 -0700

    [ add ] - adding middleware on apolloclient

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { setContext } from "apollo-link-context";
 
 import App from './components/App';
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(access_token);
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ''
+    }
+  };
+});
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql'
+});
+
 const client = new ApolloClient({
-  link: createHttpLink({
-    uri: 'http://localhost:4000/graphql'
-  }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
