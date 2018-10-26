d5c4ae04066b8003ab222a26bdda6a35867ae879
Jaehyun Chi <jaehchi@gmail.com>
2018-10-25 20:48:30 -0700

    [ add] - adding footer styling

--- client/src/components/Landing/Footer/index.jsx
+++ client/src/components/Landing/Footer/index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import './footer.sass';
+
 const Footer = () => {
   return (
-    <div>
-      hey
+    <div id="footer">
+      <div className="f-wrapper">
+        <h1 className="f-title">Ready to try Acai? It's Free!</h1>
+        <button className="f-button">
+          <a className="f-button" href="/register">Register Now!</a>
+        </button>  
+      </div>
     </div>
   );
 }
