f2647b2a5e223dacc80e7bbbb104c0546e817e4f
Jaehyun Chi <jaehchi@gmail.com>
2018-11-25 22:10:10 -0800

    [ fix ] - fixing bug regarding broken image

--- client/src/components/Home/GuildList/GuildItem/index.jsx
+++ client/src/components/Home/GuildList/GuildItem/index.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+// import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 import './guildItem.sass';
+class GuildItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      avatar: this.props.guild.avatar,
+    }
 
-const GuildItem = ({ guild: { id, name, avatar, channels }}) => {
-  return (
-    <div className="guildItem">
-      <Link to={`/${id}/${channels[0].id}`}>
-        <div className="g-avatar">
-          {
-            avatar ? <img src={avatar}/> : <span>{name[0]}</span>
-          }
-        </div>
-      </Link>
-    </div>
-  )
+    this.onBrokenImage = this.onBrokenImage.bind(this);
+  }
+
+  onBrokenImage (e) {
+    this.setState({
+      avatar: null, 
+    })
+  }
+
+  render() {
+    const { guild: { id, name, avatar, channels }} = this.props;
+    return (
+      <div className="guildItem">
+        <Link to={`/${id}/${channels[0].id}`}>
+          <div className="g-avatar">
+            {
+              this.state.avatar ? 
+                <img 
+                  src={avatar} 
+                  onError={this.onBrokenImage.bind(this)} 
+                /> : <span>{name[0]}</span>
+            }
+          </div>
+        </Link>
+      </div>
+    )
+  }
 };
 
 export default GuildItem;
