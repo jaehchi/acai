76b1e1d7da887d234e6bbacef03fe2901855de95
Jaehyun Chi <jaehchi@gmail.com>
2018-11-28 21:43:54 -0800

    [ add] - adding join guild thru invite feature

--- gql-server/src/config/graphQL/resolvers/mutations/joinGuild.js
+++ gql-server/src/config/graphQL/resolvers/mutations/joinGuild.js
@@ -1,18 +1,50 @@
 import { getUserID } from '../../../utils/jwt';
+import { decrypt } from '../../../utils/crypto';
 
-export const joinGuild = async (parent, { id }, ctx, info) => {
+export const joinGuild = async (parent, { slug }, ctx, info) => {
   const userID = await getUserID(ctx.request);
 
-  return await ctx.db.mutation.updateGuild({
+  const slugData = await ctx.db.query.slugs({
     where: {
-      id,
-    },
-    data : {
-      members: {
-        connect: {
-          id: userID
+      code: slug
+    }
+  }, `{ id, code, key guild { id }}`);
+
+  console.log(slugData)
+
+  const dec = await decrypt(slugData[0].code + slugData[0].key);
+  console.log(dec);
+
+  if ( dec === slugData[0].id ) {
+    return await ctx.db.mutation.updateGuild({
+      where: {
+        id: slugData[0].guild.id
+      },
+      data: {
+        members: {
+          connect: {
+            id: userID
+          }
         }
       }
-    }
-  }, info );
+    });
+  }
+
+  return 'Invalid invite'
+  // receive slug sfrom client
+  // transform slugs to receive guild.id
+  // update guild with it's newest member!
+  
+  // return await ctx.db.mutation.updateGuild({
+  //   where: {
+  //     id,
+  //   },
+  //   data : {
+  //     members: {
+  //       connect: {
+  //         id: userID
+  //       }
+  //     }
+  //   }
+  // }, info );
 };
