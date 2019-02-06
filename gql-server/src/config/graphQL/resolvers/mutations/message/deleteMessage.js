cdf009bd025600d5966f7088cdbfa5c966093854
Jaehyun Chi <jaehchi@gmail.com>
2019-02-05 18:00:42 -0800

    [ add] - adding delete messages/ user methods

--- gql-server/src/config/graphQL/resolvers/mutations/message/deleteMessage.js
+++ gql-server/src/config/graphQL/resolvers/mutations/message/deleteMessage.js
@@ -3,15 +3,12 @@ import { getUserID } from '../../../../utils/jwt';
 export const deleteMessage = async (parent, { id }, ctx, info) => {
   const userID = await getUserID(ctx.request);
   const where = { id };
-
-  const { author } = await ctx.db.query.message({ where }, `author { id }`);
+  const { author } = await ctx.db.query.message({ where }, `{author { id }}`);
 
   if ( author.id !== userID ) {
     throw new Error('only the author of the message can edit their messages');
   }
 
-  return await ctx.db.mutation.deleteMessage({
-    where,
-  }, info)
+  return await ctx.db.mutation.deleteMessage({ where }, info);
   
 };
--- gql-server/src/config/graphQL/resolvers/mutations/user/deleteUser.js
+++ gql-server/src/config/graphQL/resolvers/mutations/user/deleteUser.js
@@ -1,5 +1,7 @@
 import { getUserID } from '../../../../utils/jwt';
 
-export const deleteUser = async (parent, { id }, ctx, info) => {
-  
+export const deleteUser = async (parent, args, ctx, info) => {
+  const userID = getUserID(ctx.request);
+
+  return await ctx.db.mutation.deleteUser({ where: { id: userID }}, info);
 };
